757d034945f63fcbb02ff6dcb575ee7184223c83
todtk <batkatod#gmail.com>
2022-06-23 22:44:08 +0500

    hw7 task2 complete

--- hw_7/main.py
+++ hw_7/main.py
@@ -4,15 +4,14 @@ import os
 from pprint import pprint
 
 
-
 def search_path():
     FILE_NAME = "recipes.txt"
     BASE_PATH = os.getcwd()
     FULL_PATH = os.path.join(BASE_PATH, FILE_NAME)
     return FULL_PATH
 
-
 def file_reader():
+    """ЧИТАЕТ РЕЦЕПТЫ ИЗ ФАЙЛА, ГЕНЕРИРУЕТ СЛОВАРЬ ПО 1 ЗАДАЧЕ"""
     cook_book = {}
     FULL_PATH = search_path()
     with open(FULL_PATH, encoding="utf-8") as f:
@@ -27,5 +26,15 @@ def file_reader():
             cook_book[dish_name] = ingredients
     return cook_book
 
+def get_shop_list_by_dishes(dishes, person_count):
+    """БЕРЕТ ВОЗВРАТ ИЗ FILE_READER И ГЕНЕРИРУЕТ СПИСОК ПОКУПОК ДЛЯ 2 ЗАДАЧИ"""
+    cook_book = file_reader()
+    shop_list = {}
+    for dish in dishes:
+        recipe = cook_book[dish]
+        for ingredient in recipe:
+            shop_list[ingredient["ingredient_name"]] = {"measure": ingredient["measure"], "quantity": (int(ingredient["quantity"])*person_count)}
+    return shop_list
 
-pprint(file_reader())
+
+pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
